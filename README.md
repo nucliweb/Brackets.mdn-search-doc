96af38a63552e98840e4d182d11ef405ee083d06
Joan Leon <joan.leon@gmail.com>
2014-07-27 11:28:53 +0200

    First version 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-Brackets.mdn-search-doc
-=======================
+## MDN Search Doc
 
-Brackets Extension for consult the documentation in the Mozilla Developer Network
+This is a [Brackets](http://brackets.io) extension for searching [MDN](http://developer.mozilla.org/en-US/search) documentation. 
+
+## Installation
+
+* Select **File > Extension Manager...** (or click the "brick" icon in the toolbar)
+* Click **Install from URL...**
+* Enter the url of this repo
+ * For Brackets Sprint 28 (or later): https://github.com/gruehle/dev-docs-viewer
+* Click **Install**
+
+## How to use
+
+Click the "M" icon in the toolbar, select **Navigate > Lookup in MDN** or press "Shift-Ctrl-M" (Wind/Linux) | "Shift-Cmd-M" (Mac) to search the [Mozilla Developer Network](http://developer.mozilla.org/en-US/search) content using the current editor selection.
+
+## Credits
+
+Thanks to [@mozilla](http://twitter.com/mozilla) for the [great content](http://developer.mozilla.org).
+
+## Version History
+
+- 27/07/2014 v0.1.0 - Initial release.
+
+##ToDo
+
+- Add settings for select language for queries.
---
+++ main.js
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2014 Joan Leon <joan.leon@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, window */
+
+define(function (require, exports, module) {
+    "use strict";
+
+    // Brackets modules
+    var CommandManager      = brackets.getModule("command/CommandManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
+        KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
+        Menus               = brackets.getModule("command/Menus"),
+        NativeApp           = brackets.getModule("utils/NativeApp"),
+        PanelManager        = brackets.getModule("view/PanelManager");
+    
+    
+    // Constants
+    var NAVIGATE_LOOKUP_IN_MDN  = "Lookup in MDN",
+        CMD_LOOKUP_IN_MDNDOC    = "nucliweb.lookupInMdnDoc";
+    
+    // jQuery objects
+    var $icon;
+    
+    // Other vars
+    var query,
+        language = "en-US";
+        
+    function _loadDocumentation() {
+        
+        var url = "http://developer.mozilla.org/"+language+"/search";
+        var editor = EditorManager.getActiveEditor();
+        
+        if (!editor) {
+            return;
+        }
+        if (!editor.hasSelection()) {
+            customSelectWordAt(editor.getSelection().start);
+            console.log(editor.selectWordAt(editor.getSelection().start));
+        }
+        query = editor.getSelectedText();
+        
+        if (query) {
+            url += "?q=" + query;
+        }
+
+        NativeApp.openURLInDefaultBrowser(url);
+        
+    }
+
+    function customSelectWordAt(pos) {
+        var line = editor.getLine(pos.line),
+            start = pos.ch,
+            end = pos.ch;
+        
+        function isWordChar(ch) {
+            return (/\w/).test(ch) || ch.toUpperCase() !== ch.toLowerCase();
+        }
+        
+        while (start > 0 && isWordChar(line.charAt(start - 1))) {
+            --start;
+        }
+        while (end < line.length && isWordChar(line.charAt(end))) {
+            ++end;
+        }
+        this.setSelection({line: pos.line, ch: start}, {line: pos.line, ch: end});
+    };
+    
+    
+    // Insert CSS for this extension
+    ExtensionUtils.loadStyleSheet(module, "MdnSearchDoc.css");
+    
+    // Add toolbar icon 
+    $icon = $("<a>")
+        .attr({
+            id: "mdnsearchdoc-viewer-icon",
+            href: "#",
+            title: "MDN Search Doc"
+        })
+        .click(_loadDocumentation)
+        .appendTo($("#main-toolbar .buttons"));
+    
+    
+    // Add "Lookup in MDN Doc" command
+    function _handleLookupInMdnDoc() {
+        _loadDocumentation();
+    }
+    
+    
+    // Register the command and shortcut
+    CommandManager.register(
+        NAVIGATE_LOOKUP_IN_MDN,
+        CMD_LOOKUP_IN_MDNDOC,
+        _handleLookupInMdnDoc
+    );
+    KeyBindingManager.addBinding(CMD_LOOKUP_IN_MDNDOC, "Shift-Ctrl-M");
+    
+    // Create a menu item bound to the command
+    var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
+    menu.addMenuItem(CMD_LOOKUP_IN_MDNDOC);
+});
